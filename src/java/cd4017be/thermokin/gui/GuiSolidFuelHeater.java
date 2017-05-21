fe047310688ec363892d9990dc9a2af4f7666886
CD4017BE <cd4017be@gmail.com>
2017-05-21 11:11:15 +0200

    change concept of SolidFuelHeater

--- src/java/cd4017be/thermokin/gui/GuiSolidFuelHeater.java
+++ src/java/cd4017be/thermokin/gui/GuiSolidFuelHeater.java
@@ -3,14 +3,20 @@ package cd4017be.thermokin.gui;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.network.PacketBuffer;
 import net.minecraft.util.ResourceLocation;
+import cd4017be.thermokin.Objects;
+import cd4017be.thermokin.physics.GasState;
 import cd4017be.thermokin.tileentity.SolidFuelHeater;
+import cd4017be.thermokin.tileentity.SolidFuelHeater.LastState;
 import cd4017be.lib.BlockGuiHandler;
+import cd4017be.lib.Gui.DataContainer;
 import cd4017be.lib.Gui.GuiMachine;
 import cd4017be.lib.Gui.TileContainer;
 import cd4017be.lib.templates.AutomatedTile;
+import cd4017be.lib.util.Utils;
 
 public class GuiSolidFuelHeater extends GuiMachine {
 
+	private static final float T_OFS = 300F, T_RANGE = 2500F;
 	private final SolidFuelHeater tile;
 
 	public GuiSolidFuelHeater(SolidFuelHeater tileEntity, EntityPlayer player) {
@@ -24,36 +30,64 @@ public class GuiSolidFuelHeater extends GuiMachine {
 		this.xSize = 176;
 		this.ySize = 150;
 		super.initGui();
-		guiComps.add(new Slider(1, 80, 24, 70, 198, 8, 4, 12, true));
-		guiComps.add(new ProgressBar(2, 54, 31-14, 14, 14, 184, 22-14, (byte)1));
-		guiComps.add(new ProgressBar(3, 53, 50-16, 8, 16, 176, 24-16, (byte)1).setTooltip("x*8+0;boiler.burnUp"));
-		guiComps.add(new ProgressBar(4, 80, 16, 70, 8, 176, 0, (byte)0));
-		guiComps.add(new Text(5, 115, 38, 0, 8, "Temp2").center().font(0x808040, 8));
-		guiComps.add(new Button(6, 62, 35, 6, 6, -1));
-		guiComps.add(new Button(7, 62, 43, 6, 6, -1));
+		guiComps.add(new Slider(1, 89, 16, 70, 180, 8, 4, 12, true));
+		guiComps.add(new Slider(2, 89, 38, 70, 176, 8, 4, 12, true));
+		guiComps.add(new ProgressBar(3, 89, 29, 70, 8, 176, 0, (byte)0));
+		guiComps.add(new ProgressBar(4, 26, 16, 4, 34, 200, 8, (byte)1));
+		guiComps.add(new AirMix(5, 44, 16, 34, 34));
+		guiComps.add(new ProgressBar(6, 54, 35, 14, 14, 184, 8, (byte)1));
+		guiComps.add(new Tooltip<Object[]>(7, 44, 16, 34, 34, "heater.mix"));
+		guiComps.add(new Tooltip<Float>(8, 89, 16, 70, 12, "thermostat.max"));
+		guiComps.add(new Tooltip<Float>(9, 89, 38, 70, 12, "thermostat.min"));
+		guiComps.add(new Tooltip<Float>(10, 89, 29, 70, 8, "Temp1"));
 	}
 
 	@Override
 	protected Object getDisplVar(int id) {
+		LastState ls = (LastState)((DataContainer)inventorySlots).extraRef;
 		switch(id) {
-		case 1: return (tile.Tref - 300F) / 2500F;
-		case 2: return (float)tile.burn / (float)tile.fuel;
-		case 3: return (float)tile.speed / 8F;
-		case 4: return (tile.temp - 300F) / 2500F;
-		case 5: return new Object[]{tile.temp - 273.15F, tile.Tref - 273.15F};
+		case 1: return (ls.Tmax - T_OFS) / T_RANGE;
+		case 2: return (ls.Tmin - T_OFS) / T_RANGE;
+		case 3: return (ls.temp - T_OFS) / T_RANGE;
+		case 4: return ls.fuel / ls.maxFuel;
+		case 5: return ls;
+		case 6: return ls.speed / 14.0F;
+		case 7: return new Object[]{ls.in.localizedName(), ls.dV * 100F, ls.Tin - 273.15F, Utils.formatNumber(ls.P, 4, 0)};
+		case 8: return ls.Tmax - 273.15F;
+		case 9: return ls.Tmin - 273.15F;
+		case 10: return ls.temp - 273.15F;
 		default: return null;
 		}
 	}
 
 	@Override
 	protected void setDisplVar(int id, Object obj, boolean send) {
+		LastState ls = (LastState)((DataContainer)inventorySlots).extraRef;
 		PacketBuffer dos = tile.getPacketTargetData();
 		switch(id) {
-		case 1: dos.writeByte(AutomatedTile.CmdOffset).writeFloat(tile.Tref = (Float)obj * 2500F + 300F); break;
-		case 6: dos.writeByte(AutomatedTile.CmdOffset + 1); break;
-		case 7: dos.writeByte(AutomatedTile.CmdOffset + 2); break;
+		case 1: dos.writeByte(AutomatedTile.CmdOffset).writeFloat(ls.Tmax = (Float)obj * T_RANGE + T_OFS); break;
+		case 2: dos.writeByte(AutomatedTile.CmdOffset + 1).writeFloat(ls.Tmin = (Float)obj * T_RANGE + T_OFS); break;
 		}
 		if (send) BlockGuiHandler.sendPacketToServer(dos);
+	}
+
+	private class AirMix extends GuiComp<GasState> {
+
+		public AirMix(int id, int px, int py, int w, int h) {
+			super(id, px, py, w, h);
+		}
+
+		@Override
+		public void draw() {
+			LastState ls = (LastState)getDisplVar(id);
+			int f = (int)(ls.dV * (float)h), f1 = h - f;
+			color(ls.in.color | 0x60000000);
+			drawTexturedModalRect(px, py + f1, 204, 8 + f1, w, f);
+			color(Objects.combustionWaste.color | 0x60000000);
+			drawTexturedModalRect(px, py, 204, 8, w, f1);
+			color(0xffffffff);
+		}
+
 	}
 
 }
--- src/java/cd4017be/thermokin/tileentity/SolidFuelHeater.java
+++ src/java/cd4017be/thermokin/tileentity/SolidFuelHeater.java
@@ -4,6 +4,7 @@ import java.io.IOException;
 
 import net.minecraft.block.Block;
 import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.init.Items;
 import net.minecraft.item.ItemStack;
 import net.minecraft.nbt.NBTTagCompound;
 import net.minecraft.network.PacketBuffer;
@@ -13,132 +14,296 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraftforge.common.capabilities.Capability;
 import net.minecraftforge.items.SlotItemHandler;
 import cd4017be.thermokin.Objects;
+import cd4017be.thermokin.multiblock.GasContainer;
+import cd4017be.thermokin.multiblock.GasPhysics.IGasCon;
 import cd4017be.thermokin.multiblock.HeatReservoir;
+import cd4017be.thermokin.physics.GasState;
+import cd4017be.thermokin.physics.Substance;
 import cd4017be.lib.Gui.DataContainer;
 import cd4017be.lib.Gui.DataContainer.IGuiData;
+import cd4017be.lib.Gui.SlotItemType;
 import cd4017be.lib.Gui.TileContainer;
 import cd4017be.lib.templates.AutomatedTile;
 import cd4017be.lib.templates.Inventory;
+import cd4017be.lib.templates.SharedNetwork;
 import cd4017be.lib.util.Utils;
 
-public class SolidFuelHeater extends AutomatedTile implements IGuiData {
+public class SolidFuelHeater extends AutomatedTile implements IGuiData, IGasCon {
 
-	public static float FuelEnergy, C0, R0;
-	private static final int BurnRate = 8;
-	public int fuel, burn, speed;
-	public float Tref, temp;
-	HeatReservoir heat;
+	private static final double V_LIMIT = 0x1p-6;
+	public static double Size;
+	public static float C0, R0;
+	/**[molR/Ft] molar density of furnace fuel */
+	public static float FuelDensity;
+	/**[molR] minimum mount of fuel to store */
+	public static float MinFuel;
+	/**[J/molR] chemical energy stored in fuel */
+	public static double BurnEnergy;
+	/**[K] minimum temperature for self-ignition */
+	public static float IgnitionTemp;
+	/**[W/K] heat conductivity: flame -> casing */
+	public static double FlameOutReact;
+	/** */
+	public static double ReactMult;
+
+	private HeatReservoir casing;
+	private GasContainer gasIn, gasOut;
+	private float Tmin, Tmax, fuel, maxFuel, speed;
+	private boolean flame = false;
 
 	public SolidFuelHeater() {
-		//ints: maxFuel, curFuel, burn; floats: targetTemp, Temp
-		heat = new HeatReservoir(C0, R0);
-		inventory = new Inventory(2, 1, null).group(0, 0, 2, Utils.IN);
-		speed = 1;
-		Tref = 300F;
+		gasIn = new GasContainer(this, Size - V_LIMIT);
+		gasOut = new GasContainer(this, V_LIMIT);
+		gasIn.con = 0b000001;
+		gasOut.con = 0b000010;
+		casing = new HeatReservoir(C0, R0);
+		inventory = new Inventory(2, 1, null).group(0, 0, 1, Utils.IN);
+
+		Tmin = 300F; Tmax = 1200F;
 	}
 
 	@Override
 	public void update() {
 		super.update();
 		if (worldObj.isRemote) return;
-		heat.update(this);
-		if (burn < speed && heat.T < Tref && !worldObj.isBlockPowered(pos)) {
-			for (int i = 0; i < 2; i++) {
-				int n = TileEntityFurnace.getItemBurnTime(inventory.items[i]);
-				if (n != 0) {
-					fuel = n;
-					burn += fuel;
-					inventory.extractItem(i, 1, false);
-					break;
+		casing.update(this);
+		gasIn.network.updateTick(gasIn);
+		gasOut.network.updateTick(gasOut);
+		final GasState gas0 = gasIn.network.gas;
+		final double inV = gasIn.V - V_LIMIT;
+		exchangePressure(gas0, gasOut.network.gas, inV);
+		if (fuel < MinFuel) consumeFuel();
+		if (fuel <= 0) flame = false;
+		else {
+			double reactivity = inV / (Size - 2D * V_LIMIT);
+			reactivity *= reactivity * gas0.T * Math.min(1, fuel / MinFuel) * ReactMult;
+			if (reactivity > inV) reactivity = inV;
+			reactivity *= gas0.nR / gas0.V * gas0.type.ox;
+			double flameOut = (IgnitionTemp - casing.T) * FlameOutReact;
+			if (reactivity < flameOut) flame = false;
+			else if (flame) {
+				//Thermostat control:
+				float t = (Tmax - casing.T) / (Tmax - Tmin);
+				if (t < 0) t = 0;
+				else if (t > 1) t = 1;
+				reactivity = Math.max((double)t * reactivity, flameOut);
+				speed = (float)(reactivity / Math.max(1e-9, flameOut));
+				//Chemical reaction:
+				if (reactivity > fuel) {
+					reactivity = fuel;
+					flame = false;
 				}
-			}
+				fuel -= reactivity; //consume fuel
+				GasState g = gas0.remove(reactivity / gas0.type.ox, 0); //consume oxidizer
+				g.T += BurnEnergy * g.type.ox; //add reaction energy
+				outputGas(g);
+			} else if (flameOut < 0) flame = true;//self ignition
+			else if (reactivity > 2 * flameOut) tryIgnition();
 		}
-		if (burn != 0) {
-			int p = Math.min(speed, burn);
-			burn -= p;
-			heat.T += p * FuelEnergy / heat.C;
+		if (!flame) speed = 0;
+	}
+
+	private void exchangePressure(GasState gas0, GasState gas1, double inV) {
+		double E0 = gas0.E(), E1 = gas1.E();
+		double dV = (E1 * gas0.V - E0 * gas1.V) / (E0 + E1);
+		boolean tooMuch = false;
+		if (dV > inV) {
+			dV = inV;
+			flame = false;
+		} else if (gasOut.V + dV < V_LIMIT) {
+			dV = V_LIMIT - gasOut.V;
+			tooMuch = true;
 		}
-		temp = heat.T;
+		if (dV != 0) {
+			gasIn.V -= dV; gas0.V -= dV;
+			gasOut.V += dV; gas1.V += dV;
+		}
+		if (tooMuch) {
+			double TV0 = gas0.T / gas0.V, TV1 = gas1.T / gas1.V;
+			outputGas(gas0.remove((gas0.nR * TV0 - gas1.nR * TV1) / (TV0 + TV1), 0));
+		}
+	}
+
+	private void consumeFuel() {
+		int n = TileEntityFurnace.getItemBurnTime(inventory.items[0]);
+		if (n != 0) {
+			fuel += FuelDensity * (float)n;
+			maxFuel = fuel;
+			inventory.extractItem(0, 1, false);
+		}
+	}
+
+	private void tryIgnition() {
+		if (fuel < MinFuel) return; //not enough fuel
+		ItemStack item = inventory.items[1];
+		if (item != null && item.getItem() == Items.FLINT_AND_STEEL) {
+			int dmg = item.getItemDamage();
+			if (dmg >= item.getMaxDamage()) inventory.items[1] = null;
+			else item.setItemDamage(dmg + 1);
+			flame = true;
+		}
+	}
+
+	private void outputGas(GasState g) {
+		//exchange heat with casing
+		casing.T = (float)(g.T = ((double)(casing.T * casing.C) + g.E()) / (g.nR + (double)casing.C));
+		//ensure that the output gas has correct type
+		gasOut.network.gas.type = Objects.combustionWaste;
+		gasOut.network.gas.add(g); //output combustion gas
 	}
 
 	@Override
 	public boolean hasCapability(Capability<?> cap, EnumFacing s) {
-		if (cap == Objects.HEAT_CAP) return true;
+		if (cap == Objects.HEAT_CAP || (cap == Objects.GAS_CAP && (s == EnumFacing.DOWN || s == EnumFacing.UP))) return true;
 		return super.hasCapability(cap, s);
 	}
 
 	@SuppressWarnings("unchecked")
 	@Override
 	public <T> T getCapability(Capability<T> cap, EnumFacing s) {
-		if (cap == Objects.HEAT_CAP) return (T)heat.getCapability(this, s);
+		if (cap == Objects.GAS_CAP) return (T)(s == EnumFacing.UP ? gasOut : s == EnumFacing.DOWN ? gasIn : null);
+		if (cap == Objects.HEAT_CAP) return (T)casing.getCapability(this, s);
 		return super.getCapability(cap, s);
 	}
 
 	@Override
 	protected void customPlayerCommand(byte cmd, PacketBuffer dis, EntityPlayerMP player) throws IOException {
-		if (cmd == 0) Tref = dis.readFloat();
-		else if (cmd == 1 && speed < BurnRate) speed++;
-		else if (cmd == 2 && speed > 1) speed--;
-	}
-
-	@Override
-	public void onNeighborBlockChange(Block b) {
-		heat.check = true;
-	}
-
-	@Override
-	public void onNeighborTileChange(BlockPos pos) {
-		heat.check = true;
+		switch (cmd) {
+		case 0:
+			Tmax = dis.readFloat();
+			if (Tmin > Tmax - 1) Tmin = Tmax - 1;
+			break;
+		case 1:
+			Tmin = dis.readFloat();
+			if (Tmax < Tmin + 1) Tmax = Tmin + 1;
+			break;
+		}
 	}
 
 	@Override
 	public void readFromNBT(NBTTagCompound nbt) {
 		super.readFromNBT(nbt);
-		heat.load(nbt, "heat");
-		Tref = nbt.getFloat("refT");
-		burn = nbt.getInteger("fuel");
-		fuel = burn;
-		speed = nbt.getInteger("burn");
+		casing.load(nbt, "heat");
+		double V = nbt.getDouble("fillV");
+		gasIn = GasContainer.readFromNBT(this, nbt, "in", V);
+		gasOut = GasContainer.readFromNBT(this, nbt, "out", Size - V);
+		gasIn.con = 0b000001;
+		gasOut.con = 0b000010;
+		Tmin = nbt.getFloat("minT");
+		Tmax = nbt.getFloat("maxT");
+		fuel = nbt.getInteger("fuel");
+		maxFuel = fuel;
+		
 	}
 
 	@Override
 	public NBTTagCompound writeToNBT(NBTTagCompound nbt) {
-		heat.save(nbt, "heat");
-		nbt.setFloat("refT", Tref);
-		nbt.setInteger("fuel", burn);
-		nbt.setInteger("burn", speed);
+		casing.save(nbt, "heat");
+		gasIn.writeToNBT(nbt, "in");
+		gasOut.writeToNBT(nbt, "out");
+		nbt.setFloat("minT", Tmin);
+		nbt.setFloat("maxT", Tmax);
+		nbt.setFloat("fuel", fuel);
 		return super.writeToNBT(nbt);
 	}
 
 	@Override
+	public void onNeighborBlockChange(Block b) {
+		casing.check = true;
+	}
+
+	@Override
+	public void onNeighborTileChange(BlockPos pos) {
+		casing.check = true;
+		gasIn.updateCon = true;
+		gasOut.updateCon = true;
+	}
+
+	@Override
+	public void validate() {
+		super.validate();
+		long uid = SharedNetwork.ExtPosUID(pos, dimensionId);
+		gasIn.setUID(uid);
+		gasOut.setUID(uid^0x8000000000000000L);
+		//gasOut.network.gas.type = Objects.combustionWaste;
+	}
+
+	@Override
+	public void invalidate() {
+		super.invalidate();
+		if (gasIn.network != null) gasIn.network.remove(gasIn);
+		if (gasOut.network != null) gasOut.network.remove(gasOut);
+	}
+
+	@Override
+	public void onChunkUnload() {
+		super.onChunkUnload();
+		if (gasIn.network != null) gasIn.network.remove(gasIn);
+		if (gasOut.network != null) gasOut.network.remove(gasOut);
+	}
+
+	@Override
 	public void initContainer(DataContainer cont) {
-		TileContainer container = (TileContainer)cont;
-		container.addItemSlot(new SlotItemHandler(inventory, 0, 26, 16));
-		container.addItemSlot(new SlotItemHandler(inventory, 1, 26, 34));
+		TileContainer container = (TileContainer)cont;		
+		container.addItemSlot(new SlotItemHandler(inventory, 0, 8, 16));
+		container.addItemSlot(new SlotItemType(inventory, 1, 8, 34, new ItemStack(Items.FLINT_AND_STEEL)));
 		container.addPlayerInventory(8, 68);
+		
+		cont.extraRef = new LastState();
 	}
 
 	@Override
 	public boolean transferStack(ItemStack item, int s, TileContainer container) {
 		if (s < container.invPlayerS) container.mergeItemStack(item, container.invPlayerS, container.invPlayerE, false);
-		else container.mergeItemStack(item, 0, 2, false);
+		else container.mergeItemStack(item, 0, 2, true);
 		return true;
 	}
 
 	@Override
-	public int[] getSyncVariables() {
-		return new int[]{fuel, burn, speed, Float.floatToIntBits(Tref), Float.floatToIntBits(temp)};
+	public boolean detectAndSendChanges(DataContainer cont, PacketBuffer dos) {
+		LastState ls = (LastState)cont.extraRef;
+		dos.writeFloat(casing.T);
+		GasState gas0 = gasIn.network.gas;
+		dos.writeFloat((float)gas0.T);
+		dos.writeFloat((float)gas0.P());
+		dos.writeFloat((float)((gasIn.V - V_LIMIT) / (Size - 2D * V_LIMIT)));
+		int p = dos.writerIndex();
+		byte chng = 0;
+		dos.writeByte(chng);
+		if (fuel != ls.fuel) {dos.writeFloat(ls.fuel = fuel); chng |= 1;}
+		if (maxFuel != ls.maxFuel) {dos.writeFloat(ls.maxFuel = maxFuel); chng |= 2;}
+		if (speed != ls.speed) {dos.writeFloat(ls.speed = speed); chng |= 4;}
+		if (Tmin != ls.Tmin) {dos.writeFloat(ls.Tmin = Tmin); chng |= 8;}
+		if (Tmax != ls.Tmax) {dos.writeFloat(ls.Tmax = Tmax); chng |= 16;}
+		if (gas0.type != ls.in) {dos.writeInt(Substance.getId(ls.in = gas0.type)); chng |= 32;}
+		if (chng != 0) dos.setByte(p, chng);
+		return true;
 	}
 
 	@Override
-	public void setSyncVariable(int i, int v) {
-		switch(i) {
-		case 0: fuel = v; break;
-		case 1: burn = v; break;
-		case 2: speed = v; break;
-		case 3: Tref = Float.intBitsToFloat(v); break;
-		case 4: temp = Float.intBitsToFloat(v); break;
-		}
+	public void updateClientChanges(DataContainer cont, PacketBuffer dis) {
+		LastState ls = (LastState)cont.extraRef;
+		ls.temp = dis.readFloat();
+		ls.Tin = dis.readFloat();
+		ls.P = dis.readFloat();
+		ls.dV = dis.readFloat();
+		byte chng = dis.readByte();
+		if ((chng & 1) != 0) ls.fuel = dis.readFloat();
+		if ((chng & 2) != 0) ls.maxFuel = dis.readFloat();
+		if ((chng & 4) != 0) ls.speed = dis.readFloat();
+		if ((chng & 8) != 0) ls.Tmin = dis.readFloat();
+		if ((chng & 16) != 0) ls.Tmax = dis.readFloat();
+		if ((chng & 32) != 0) ls.in = Substance.REGISTRY.getObjectById(dis.readInt());
+	}
+
+	public static class LastState {
+		public float fuel, maxFuel, speed, Tmin, Tmax, temp, dV, Tin, P;
+		public Substance in = Substance.Default;
+	}
+
+	@Override
+	public boolean conGas(byte side) {
+		return side == 0 || side == 1;
 	}
 
 }
